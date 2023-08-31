94dbb9711f27de2a6064be841127681843577127
6250zed <117414591+6250zed@users.noreply.github.com>
2023-08-31 15:05:00 -0400

    Update Main.rs
    Rebinded keys now work, but still allow original functionality. This needs fixing!

--- Main.rs
+++ Main.rs
@@ -1,71 +1,23 @@
-#[cfg(feature = "winit")]
-mod window_handler {
-    use winit::{
-        event::*,
-        event_loop::{ControlFlow, EventLoop},
-        window::WindowBuilder,
-    };
+use inputbot::{KeybdKey, MouseButton};
 
-    pub fn run_event_loop() {
-        let event_loop = EventLoop::<()>::new();
-        let window_builder = WindowBuilder::new().with_title("buttons");
-        let window = window_builder.build(&event_loop).expect("Failed to create window");
-
-        let mut keyboard = buttons::support::winit::keyboard();
-        let mut mouse = buttons::support::winit::mouse();
-
-        // Maintain a state for remapping keys to mouse buttons
-        let mut remapping_a_to_mouse_left = false;
-        let mut remapping_d_to_mouse_right = false;
-
-        event_loop.run(move |event, _, control_flow| {
-            keyboard.handle_event(&event);
-            mouse.handle_event(&event);
-
-            match event {
-                Event::WindowEvent { event, .. } => match event {
-                    WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
-                    _ => (),
-                },
-                Event::MainEventsCleared => {
-                    window.request_redraw();
-
-                    // Check if the "A" key is pressed, toggle remapping for left mouse button
-                    if keyboard.pressed(VirtualKeyCode::A) {
-                        remapping_a_to_mouse_left = !remapping_a_to_mouse_left;
-                    }
-
-                    // Check if the "D" key is pressed, toggle remapping for right mouse button
-                    if keyboard.pressed(VirtualKeyCode::D) {
-                        remapping_d_to_mouse_right = !remapping_d_to_mouse_right;
-                    }
-
-                    // Simulate mouse button presses based on key remapping
-                    if remapping_a_to_mouse_left {
-                        mouse.press(MouseButton::Left);
-                    }
-                    if remapping_d_to_mouse_right {
-                        mouse.press(MouseButton::Right);
-                    }
-
-                    // Printing code remains unchanged
-
-                    keyboard.clear_presses();
-                    mouse.clear_presses();
-
-                    std::thread::sleep(std::time::Duration::from_millis(16)); // Adjust sleep duration
-                }
-                Event::RedrawRequested(_) => {}
-                _ => (),
-            }
-        });
-    }
-}
-
-#[cfg(feature = "winit")]
 fn main() {
-    window_handler::run_event_loop();
-}
+    // Bind all keys to a common callback event.
+    KeybdKey::bind_all(|event| {
+        match event {
+            KeybdKey::WKey => {
+                // Simulate right-click when "w" is pressed
+                MouseButton::RightButton.press();
+                MouseButton::RightButton.release();
+            }
+            KeybdKey::AKey => {
+                // Simulate "a" key when left mouse button is pressed
+                MouseButton::LeftButton.press();
+                MouseButton::LeftButton.release();
+            }
+            _ => {}
+        }
+    });
 
-#[cfg(not(feature = "winit"))]
-fn main() {}
+    // Call this to start listening for bound inputs.
+    inputbot::handle_input_events();
+}
