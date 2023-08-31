0e11f1ef28f7be51b4937d0d40f5631bd4e0ca3b
6250zed <117414591+6250zed@users.noreply.github.com>
2023-08-31 15:06:29 -0400

    Update Main.rs
    Original key disabled while allowing new funcionality to exisit.

--- Main.rs
+++ Main.rs
@@ -1,23 +1,12 @@
-use inputbot::{KeybdKey, MouseButton};
-
-fn main() {
-    // Bind all keys to a common callback event.
-    KeybdKey::bind_all(|event| {
-        match event {
-            KeybdKey::WKey => {
-                // Simulate right-click when "w" is pressed
-                MouseButton::RightButton.press();
-                MouseButton::RightButton.release();
+use inputbot::{KeybdKey, MouseButton}; fn main() { // Bind all keys to a common callback event. KeybdKey::bind_all(|event| { match event {
+            KeybdKey::WKey => { // Simulate right-click when "w" is pressed MouseButton::Right.press();
+                MouseButton::Right.release();
             }
-            KeybdKey::AKey => {
-                // Simulate "a" key when left mouse button is pressed
-                MouseButton::LeftButton.press();
-                MouseButton::LeftButton.release();
+            KeybdKey::AKey => { // Simulate "a" key when left mouse button is pressed MouseButton::Left.press();
+                MouseButton::Left.release();
             }
-            _ => {}
+            _ => { // Pass through the original key event KeybdKey::send(event);
+            }
         }
-    });
-
-    // Call this to start listening for bound inputs.
-    inputbot::handle_input_events();
+    }); // Call this to start listening for bound inputs. inputbot::handle_input_events();
 }
