85ecb7c57d98ccc8f786a700fe4b3b31da647f77
6250zed <117414591+6250zed@users.noreply.github.com>
2023-08-31 15:08:47 -0400

    Update3 Main.rs
    Disable orininal key whilst still allowing new functionality. Prior attempt was old code.

--- Main.rs
+++ Main.rs
@@ -1,12 +1,26 @@
-use inputbot::{KeybdKey, MouseButton}; fn main() { // Bind all keys to a common callback event. KeybdKey::bind_all(|event| { match event {
-            KeybdKey::WKey => { // Simulate right-click when "w" is pressed MouseButton::Right.press();
+use inputbot::{KeybdKey, MouseButton};
+
+fn main() {
+    // Bind all keys to a common callback event.
+    KeybdKey::bind_all(|event| {
+        match event {
+            KeybdKey::WKey => {
+                // Simulate right-click when "w" is pressed
+                MouseButton::Right.press();
                 MouseButton::Right.release();
             }
-            KeybdKey::AKey => { // Simulate "a" key when left mouse button is pressed MouseButton::Left.press();
+            KeybdKey::AKey => {
+                // Simulate "a" key when left mouse button is pressed
+                MouseButton::Left.press();
                 MouseButton::Left.release();
             }
-            _ => { // Pass through the original key event KeybdKey::send(event);
+            _ => {
+                // Pass through the original key event
+                KeybdKey::send(event);
             }
         }
-    }); // Call this to start listening for bound inputs. inputbot::handle_input_events();
+    });
+
+    // Call this to start listening for bound inputs.
+    inputbot::handle_input_events();
 }
